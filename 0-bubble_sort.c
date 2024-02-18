31db136a1241677d22a40655cdfbe383cad6f775
userkw <elkis.kawthar11@gmail.com>
2024-02-18 12:34:27 -0800

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,31 +8,31 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int temp;
+	int swapped;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
 
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
 
-        if (swapped == 0)
-            break;
-    }
+		if (swapped == 0)
+			break;
+	}
 }
